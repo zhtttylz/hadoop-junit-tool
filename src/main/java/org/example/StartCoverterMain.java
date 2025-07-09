5bd27ecd6d894efdb181a65c98211ef344580083
zhtttylz <hualong.z@hotmail.com>
2025-07-09 10:31:43 +0800

    Checkstyle auto-formatting

--- src/main/java/org/example/StartCoverterMain.java
+++ src/main/java/org/example/StartCoverterMain.java
@@ -13,9 +13,11 @@ public class StartCoverterMain {
 
   static String converterDir = "/Users/didi/IdeaProjects/hadoop/hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/DFSTestUtil.java";
 
+  static String repoRoot = "/Users/didi/IdeaProjects/hadoop";
+
   public static void main(String[] args) throws IOException {
     List<String> files = ModifyFileCmd.getModifyFile(scriptPath, converterDir);
-    JUnit4ToJUnit5Converter converter = new JUnit4ToJUnit5Converter();
+    JUnit4ToJUnit5Converter converter = new JUnit4ToJUnit5Converter(Path.of(repoRoot));
 
     for (String file : files) {
       System.out.println(file);
--- src/main/java/org/example/converter/JUnit4ToJUnit5Converter.java
+++ src/main/java/org/example/converter/JUnit4ToJUnit5Converter.java
@@ -5,6 +5,7 @@ import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.printer.lexicalpreservation.LexicalPreservingPrinter;
 import org.example.converter.processor.*;
 import org.example.converter.processor.AssertMethodCallFormatter;
+import org.example.util.GitDiffCodeFormatter;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -16,13 +17,16 @@ public class JUnit4ToJUnit5Converter {
 
   public static String fileName = "/Users/didi/IdeaProjects/hadoop/hadoop-hdfs-project/hadoop-hdfs-rbf/src/test/java/org/apache/hadoop/hdfs/server/federation/router/TestRouterAdminCLI.java";
 
+  final Path repoPath;
+
   // 需要交换两个参数
   final Set<String> swapTwoArgsMethods;
 
   // 需要交换三个参数
   final Set<String> shiftThreeArgsMethods;
 
-  public JUnit4ToJUnit5Converter() {
+  public JUnit4ToJUnit5Converter(Path repoPath) {
+    this.repoPath = repoPath;
     shiftThreeArgsMethods = Set.of(
         "assertEquals",
         "assertNotEquals",
@@ -80,10 +84,15 @@ public class JUnit4ToJUnit5Converter {
     String result = LexicalPreservingPrinter.print(cu);
     result = AssertMethodCallFormatter.format(result);
     Files.writeString(path, result);
+    try {
+      GitDiffCodeFormatter.formatChangedLines(repoPath, path);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 
   public static void main(String[] args) throws IOException {
-    JUnit4ToJUnit5Converter converter = new JUnit4ToJUnit5Converter();
+    JUnit4ToJUnit5Converter converter = new JUnit4ToJUnit5Converter(Path.of("."));
     converter.converter(Path.of(fileName));
   }
 }
